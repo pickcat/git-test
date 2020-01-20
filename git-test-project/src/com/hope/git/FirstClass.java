3689003b8f383bd0510f7c931c29fa634f6766ba
pickcat <254019730@qq.com>
2020-01-20 21:22:06 +0800

    b把本地工程提交

---
+++ git-test-project/src/com/hope/git/FirstClass.java
@@ -0,0 +1,13 @@
+package com.hope.git;
+
+/**
+ * @author newcityman
+ * @date 2020/1/20 - 21:14
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+    }
+}
