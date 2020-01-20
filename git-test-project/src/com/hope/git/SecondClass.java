c5211fd574c15744d592feaa011950f5933d077c
pickcat <254019730@qq.com>
2020-01-20 21:40:58 +0800

    添加second方法

---
+++ git-test-project/src/com/hope/git/SecondClass.java
@@ -0,0 +1,11 @@
+package com.hope.git;
+
+/**
+ * @author newcityman
+ * @date 2020/1/20 - 21:40
+ */
+public class SecondClass {
+    public void test(){
+        System.out.println("second");
+    }
+}
